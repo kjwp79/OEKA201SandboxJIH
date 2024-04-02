1188edcf3937ecd41b0362b88d974a80aaa9c5d5
joernih <jorn.halvorsen@gmail.com>
2024-04-02 22:48:55 +0200

    template

---
+++ lectures/lecture5_script.R
@@ -0,0 +1,40 @@
+#devtools::install_github("joernih/OEKA201WASMP")
+library(OEKA201WASMP)
+library(dplyr)
+library(lmtest)
+library(tseries)
+##################################################################################################################
+data(package="OEKA201WASMP")$results[,3]
+#  [1] "adverticing"   "biler"         "df1"           "elbiler"       "fred_ts"       "icecream"     
+#  [7] "iris"          "iris (mydata)" "md"            "nb_ts"         "nbjson"        "newcars"      
+# [13] "tssales"       "usedvw"       
+##################################################################################################################
+### Problems set 3: linear regression ###
+# 1. Model specifications
+# Markdown syntax
+# Formula class objects in R
+# 2. Data import and transformations
+# Import data
+# Data transformations
+# 3. Descriptiv statistics (graphical and numerical)
+# 4. Model estimations
+# 5. Results
+##################################################################################################################
+### Problems set 5: non-linear regression ###
+# 1. Model specifications
+# Markdown syntax
+# Formula class objects in R
+# 2. Data import and transformations
+# 3. Descriptiv statistics (graphical and numerical)
+# 4. Model estimations
+# 5. Results
+##################################################################################################################
+### Lecture notes week 12: time series ### 
+# 1. Model specifications
+# 2. Data import and transformations
+# 3. Descriptiv statistics (graphical and numerical)
+# 4. Model estimations
+# 5. Results
+###################################################################################################################
+
+
