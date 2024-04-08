68911c4bd8274ba5aed962c476659787b2aeaa6d
joernih <jorn.halvorsen@gmail.com>
2024-04-08 14:53:26 +0200

    templated

---
+++ assignment/QandA.R
@@ -0,0 +1,25 @@
+###################################################################################################################
+## Exercise 1: ###
+# 1. Model specifications
+# Markdown syntax
+# Formula class objects in R
+# 2. Data import and transformations
+# Import data
+# Data transformations
+# 3. Descriptiv statistics (graphical and numerical)
+# 4. Model estimations
+# 5. Results
+##################################################################################################################
+## Exercise 1: ###
+# 1. Model specifications
+# Markdown syntax
+# Formula class objects in R
+# 2. Data import and transformations
+# Import data
+# Data transformations
+# 3. Descriptiv statistics (graphical and numerical)
+# 4. Model estimations
+# 5. Results
+#################################################################################################################
+
+
