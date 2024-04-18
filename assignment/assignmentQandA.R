f6f63123af8dac1abb3aaa4a3ba8817ad8b94c9e
joernih <jorn.halvorsen@gmail.com>
2024-04-18 14:54:56 +0200

    updsand

--- assignment/assignmentQandA.R
+++ assignment/assignmentQandA.R
@@ -1,26 +1,37 @@
 ###################################################################################################################
+library(dplyr)
+library(broom)
+###################################################################################################################
+## Assign dataset for exercise to variable usedvw
+usedvw <- OEKA201Assignment::usedvw
+names(usedvw)
+###################################################################################################################
 ## Exercise 1: ###
 # 1. Model specifications
 # Markdown syntax
+ex1mod1la <- "price_i=\\beta_0+\\beta_1 mileage_i + u_i"
+ex2mod1la <- "price_i=\\beta_0+\\beta_1 mileage_i+ \\beta_2 mileage_i^2 + u_i"
 # Formula class objects in R
+ex1mod1ro <- price ~ mileage
+ex2mod1ro <- price ~ mileage+mileage2
 # 2. Data import and transformations
 # Import data
+selusedvw <- usedvw %>%
+	dplyr::select(price,mileage) %>%
+	dplyr::mutate(mileage2=mileage*mileage)
 # Data transformations
 # 3. Descriptiv statistics (graphical and numerical)
+descsum <- summary(usedvw)
+desccor <- cor(usedvw)
 # 4. Model estimations
+esm1 <- lm(ex1mod1ro,data=selusedvw)
+esm2 <- lm(ex2mod1ro,data=selusedvw)
 # 5. Results
+## Mod 1
+tidres1 <- broom::tidy(esm1)
+glares1 <- broom::glance(esm1)
+## Mod 2
+tidres2 <- broom::tidy(esm2)
+glares2 <- broom::glance(esm2)
 ##################################################################################################################
-## Exercise 2: ###
-# 1. Model specifications
-# Markdown syntax
-# Formula class objects in R
-# 2. Data import and transformations
-# Import data
-# Data transformations
-# 3. Descriptiv statistics (graphical and numerical)
-# 4. Model estimations
-# 5. Results
-#################################################################################################################
 
-
-print("Hello")
